00ef1e6b1489ddc5a8937d48438333f1cdcbf141
Lucas <lucasdavidferrero@gmail.com>
2025-09-17 22:36:05 -0300

    17-09-2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
 import { ProductoModule } from './modules/producto/producto.module';
-
+import { TypeOrmModule } from "@nestjs/typeorm"
 @Module({
-  imports: [ProductoModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    ProductoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
