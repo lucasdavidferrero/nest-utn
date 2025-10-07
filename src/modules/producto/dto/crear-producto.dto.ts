8ff0a316ae2d9a6257e36f8cb5b7b5b50c0e1403
lucasdavidferrero <lucasdavidferrero@gmail.com>
2025-10-06 22:35:21 -0300

    06-10-25

--- src/modules/producto/dto/crear-producto.dto.ts
+++ src/modules/producto/dto/crear-producto.dto.ts
@@ -3,7 +3,9 @@ import {
   MinLength,
   MaxLength,
   IsDecimal,
+  Min
 } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CrearProductoDto {
     @MinLength(10)
@@ -11,11 +13,13 @@ export class CrearProductoDto {
     titulo: string
 
     @IsDecimal()
+    @Type(() => Number)
     precio: string
 
     @MinLength(5)
     slug: string
 
     @IsInt()
+    @Min(0, { message: "Ingrese un stock válido >= 0" })
     stock: number
 }
